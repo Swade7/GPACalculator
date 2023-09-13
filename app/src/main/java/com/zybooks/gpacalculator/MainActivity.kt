789a5e8fd34f1cc4af13ba8648478bfba1bbc1f9
Swade7 <swade7@harding.edu>
2023-09-13 14:53:31 -0500

    Added UI

--- app/src/main/java/com/zybooks/gpacalculator/MainActivity.kt
+++ app/src/main/java/com/zybooks/gpacalculator/MainActivity.kt
@@ -2,10 +2,32 @@ package com.zybooks.gpacalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var numAsEditText: EditText
+    private lateinit var numBsEditText: EditText
+    private lateinit var numCsEditText: EditText
+    private lateinit var numDsEditText: EditText
+    private lateinit var numFsEditText: EditText
+    private lateinit var GPA: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        numAsEditText = findViewById(R.id.AInput)
+        numBsEditText = findViewById(R.id.BInput)
+        numCsEditText = findViewById(R.id.CInput)
+        numDsEditText = findViewById(R.id.DInput)
+        numFsEditText = findViewById(R.id.FInput)
     }
+
+    fun calculateGPA(view: View) {
+
+    }
+
+
+
 }
